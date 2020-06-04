c4ec7068922f9eb3c1a4e029188e2daaaf7409db
manpreet-singh10196 <manpreet.singh10196@gmail.com>
2020-06-04 14:59:47 +0530

    add server file

--- p4-evaluate-a-news-article-with-nlp/src/server/index.js
+++ p4-evaluate-a-news-article-with-nlp/src/server/index.js
@@ -2,7 +2,24 @@ var path = require('path')
 const express = require('express')
 const mockAPIResponse = require('./mockAPI.js')
 
+
+var aylien = require('aylien_textapi');
+const bodyParser = require('body-parser');
+
+const dotenv = require('dotenv');
+dotenv.config();
+
+const textapi = new aylien({
+    application_id: `${process.env.API_ID}`,
+    application_key: `${process.env.API_KEY}`
+});
+
 const app = express()
+
+/* Middleware*/
+//Here we are configuring express to use body-parser as middle-ware.
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 app.use(express.static('dist'))
 
@@ -10,7 +27,7 @@ console.log(__dirname)
 
 app.get('/', function (req, res) {
     // res.sendFile('dist/index.html')
-    res.sendFile(path.resolve('src/client/views/index.html'))
+    res.sendFile('dist/index.html')
 })
 
 // designates what port the app will listen to for incoming requests
@@ -21,3 +38,25 @@ app.listen(8080, function () {
 app.get('/test', function (req, res) {
     res.send(mockAPIResponse)
 })
+
+
+app.post('/testing', async (req, res, next) => {
+    //console.log(req.body);
+    try {
+      var data = textapi.sentiment({
+        //'text': 'John is a very good football player!'
+        'text': req.body.theText
+      }, function(error, response) {
+        if (error === null) {
+          console.log(response);
+          res.send(response);
+        }
+      });
+      //res.send(mockAPIResponse)
+    } catch(error) {
+      // Passes errors into the handler
+      return next(error)
+    }
+    //res.send(returnVal);
+  })
+  
