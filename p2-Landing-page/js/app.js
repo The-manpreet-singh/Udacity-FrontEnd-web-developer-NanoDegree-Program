cb0f4616ffbaf26b886335a81e9fa996b50deb93
The-manpreet-singn <manpreet.singh10196@gmail.com>
2020-07-16 21:36:27 +0530

    add app

--- p2-Landing-page/js/app.js
+++ p2-Landing-page/js/app.js
@@ -18,3 +18,21 @@ sectionNav.forEach((element, index) => {
 	liTag.innerHTML = `<a onClick="scrollToSection(${toOffSection})">${sectionName}</a>`;
 	NavTag.appendChild(liTag);
 });
+document.addEventListener("scroll", () => {
+	currentPosition = this.scrollY;
+	// Section Positions
+	sectionNavPositions = [];
+	sectionNav.forEach((element) => sectionNavPositions.push(element.getBoundingClientRect().top + 50));
+
+	// Adding and removing active sections
+	let addIndex = sectionNavPositions.findIndex((element) => element > 0);
+	for (let i = 0; i < sectionNavLength; i++) {
+		if (addIndex === i) {
+			document.querySelector(".menu_link" + addIndex).classList.add("active");
+			document.querySelector(`#section${addIndex + 1}`).classList.add("current-active-class");
+		} else {
+			document.querySelector(".menu_link" + i).classList.remove("active");
+			document.querySelector(`#section${i + 1}`).removeAttribute("class");
+		}
+	}
+});
