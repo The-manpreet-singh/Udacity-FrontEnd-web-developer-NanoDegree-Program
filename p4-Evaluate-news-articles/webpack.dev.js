e381d3dfbdd1d233d9b30b3a5d80cd2bcebfb3f4
The-manpreet-singn <manpreet.singh10196@gmail.com>
2020-07-18 22:13:54 +0530

    add webpack

---
+++ p4-Evaluate-news-articles/webpack.dev.js
@@ -0,0 +1,47 @@
+const path = require('path')
+const webpack = require('webpack')
+const HtmlWebPackPlugin = require("html-webpack-plugin")
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const WorkboxPlugin = require('workbox-webpack-plugin');
+
+module.exports = {
+    mode: 'production',
+    devtool: 'source-map',
+    entry: './src/client/index.js',
+    output: {
+        libraryTarget: 'var',
+        library: 'Client'
+    },
+    module: {
+        rules: [
+            {
+                test: '/\.js$/',
+                exclude: /node_modules/,
+                loader: "babel-loader"
+            },
+            {
+               test: /\.scss$/,
+               use: [ 'style-loader', 'css-loader', 'sass-loader' ]
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebPackPlugin({
+            template: "./src/client/views/index.html",
+            filename: "./index.html",
+        }),
+        new CleanWebpackPlugin({
+            // Simulate the removal of files
+            dry: true,
+            // Write Logs to Console
+            verbose: true,
+            // Automatically remove all unused webpack assets on rebuild
+            cleanStaleWebpackAssets: true,
+            protectWebpackAssets: false
+        }),
+        new WorkboxPlugin.GenerateSW({
+            clientsClaim: true,
+            skipWaiting: true
+        }), 
+    ]
+}
---
+++ p4-Evaluate-news-articles/webpack.prod.js
@@ -0,0 +1,43 @@
+const path = require("path");
+const webpack = require("webpack");
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+const TerserPlugin = require("terser-webpack-plugin");
+const WorkboxPlugin = require("workbox-webpack-plugin");
+
+module.exports = {
+	entry: "./src/client/index.js",
+	mode: "production",
+	optimization: {
+		minimizer: [new TerserPlugin({}), new OptimizeCSSAssetsPlugin({})],
+	},
+	output: {
+		libraryTarget: "var",
+		library: "Client",
+	},
+	module: {
+		rules: [
+			{
+				test: "/.js$/",
+				exclude: /node_modules/,
+				loader: "babel-loader",
+			},
+			{
+				test: /\.scss$/,
+				use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+			},
+		],
+	},
+	plugins: [
+		new HtmlWebPackPlugin({
+			template: "./src/client/views/index.html",
+			filename: "./index.html",
+		}),
+		new MiniCssExtractPlugin({ filename: "[name].css" }),
+		new WorkboxPlugin.GenerateSW({
+			clientsClaim: true,
+			skipWaiting: true,
+		}),
+	],
+};
