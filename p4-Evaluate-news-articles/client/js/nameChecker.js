c0e27ca718b5d5f4315d52c87a777f7de25506db
The-manpreet-singn <manpreet.singh10196@gmail.com>
2020-07-18 22:23:33 +0530

    add namechecker

---
+++ p4-Evaluate-news-articles/client/js/nameChecker.js
@@ -0,0 +1,10 @@
+function checkForName(inputText) {
+	console.log("::: Running checkForName :::", inputText);
+	let names = ["Picard", "Janeway", "Kirk", "Archer", "Georgiou"];
+
+	if (names.includes(inputText)) {
+		alert("Welcome, hy");
+	}
+}
+
+export { checkForName };
