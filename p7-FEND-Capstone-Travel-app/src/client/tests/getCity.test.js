edde2502641bdfbe51aa597c67559fc9a73ce0f6
The-manpreet-singn <manpreet.singh10196@gmail.com>
2020-07-19 22:51:08 +0530

    add getcity

---
+++ p7-FEND-Capstone-Travel-app/src/client/tests/getCity.test.js
@@ -0,0 +1,11 @@
+import { getCity } from "../js/countDown";
+
+test("should return Paris", () => {
+	beforeEach(() => {
+		document.body.innerHTML = '<div id="city">' + "paris" + "</div>";
+	});
+	afterEach(() => {
+		const city = getCity();
+		expect(city).toEqual("Paris");
+	});
+});
